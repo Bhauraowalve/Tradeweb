ba2d49562b0a6038c8dc7db274a4189e3366d80d
unknown <gaurav.singh@indeftts.com>
2024-04-09 22:15:05 +0530

    first commit

--- tradeApp/asgi.py
+++ tradeApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for salonApp project.
+ASGI config for tradeApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salonApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tradeApp.settings')
 
 application = get_asgi_application()
